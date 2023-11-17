9d68d0c54048faec2e5a709751d61fdc4e12c2e0
kiakramun <149653549+kiakramun@users.noreply.github.com>
2023-11-17 16:52:04 +0100

    Update pixel.h

--- lab1/source/image/pixel.h
+++ lab1/source/image/pixel.h
@@ -9,3 +9,45 @@ enum class ChannelOrder {
 	RBG,
 	RGB
 };
+
+template <typename T>
+class Pixel {
+    public:
+        // Default constructor
+        Pixel() 
+            : blue_channel(0), green_channel(0), red_channel(0){};
+
+        // Constructor with values (assuming BGR channel order)
+        Pixel(T blue, T green, T red)
+            : blue_channel(blue), green_channel(green), red_channel(red) {};
+
+        // Public methods to get blue channel intensities
+        [[nodiscard]] T get_blue_channel() const noexcept {
+            return blue_channel;
+        }
+
+        // Public methods to get green channel intensities
+        [[nodiscard]] T get_green_channel() const noexcept {
+            return green_channel;
+        }
+
+        // Public methods to get red channel intensities
+        [[nodiscard]] T get_red_channel() const noexcept {
+            return red_channel;
+        }
+
+        // Static attribute for channel order
+        static const ChannelOrder channel_order = ChannelOrder::BGR;
+
+        // Equality operator 
+        [[nodiscard]] bool operator==(const Pixel<T>& other) const noexcept {
+            return (blue_channel == other.blue_channel) 
+                && (green_channel == other.green_channel) 
+                && (red_channel == other.red_channel);
+        }
+    private:
+        // Private attributes
+        T blue_channel;
+        T green_channel;
+        T red_channel;
+};
