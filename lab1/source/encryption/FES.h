06d906bae57274be392fa261e07ff9dab9b6d366
DiepAnhLP <92036220+DiepAnhLP@users.noreply.github.com>
2023-11-21 00:38:07 +0100

    Update FES.h

--- lab1/source/encryption/FES.h
+++ lab1/source/encryption/FES.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "Key.h"
+#include "../image/bitmap_image.h"
+#include "../image/pixel.h"
+#include "../io/image_parser.h"
+
+
+class FES{
+    // public field
+public:
+    // Constructor
+    FES(){};
+
+    // Define dependent types
+    using key_type = Key::key_type;
+    using row_type = std::array<BitmapImage::BitmapPixel, 16>;
+    using block_type = std::array<std::array<row_type, 3>, 3>;
+
+
+
+};
